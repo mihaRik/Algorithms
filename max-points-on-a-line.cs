549ddf0fc34e63f5d432704e0cd8a8f064260ed6
Rahim Teymurov <miharik.rt@gmail.com>
2022-06-15 16:46:06 +0300

    problem solved

--- max-points-on-a-line.cs
+++ max-points-on-a-line.cs
@@ -52,183 +52,76 @@ namespace Algorithms
                 new[] { -7, 7 }
             };
 
-            // Console.WriteLine(MaxPoints(testpoints).ToString());
-            // Console.WriteLine(MaxPoints(testpoints2).ToString());
-            // Console.WriteLine(MaxPoints(testpoints3).ToString());
-            // Console.WriteLine(MaxPoints(testpoints4).ToString());
-            // Console.WriteLine(MaxPoints(testpoints5).ToString());
+            Console.WriteLine(MaxPoints(testpoints).ToString());
+            Console.WriteLine(MaxPoints(testpoints2).ToString());
+            Console.WriteLine(MaxPoints(testpoints3).ToString());
+            Console.WriteLine(MaxPoints(testpoints4).ToString());
+            Console.WriteLine(MaxPoints(testpoints5).ToString());
             Console.WriteLine(MaxPoints(testpoints6).ToString());
         }
 
         static int MaxPoints(int[][] points)
         {
-            ValidateParam(points.Length, 0, 300);
-            var (maxPlaneLimit, minPlaneLimit) = FindMaxAndMinLimitsOfPlane(points);
-            var maxPointsCount = 0;
+            var maxPointsCount = 1;
 
             for (var i = 0; i < points.Length; i++)
             {
-                var currentPoint = points[i];
+                var (currentX, currentY) = GetCords(points[i]);
 
-                var pointsCount = FindLineForPoint(currentPoint, points, 0, 0, maxPlaneLimit, minPlaneLimit);
-
-                if (pointsCount > maxPointsCount)
+                for (var j = 0; j < points.Length; j++)
                 {
-                    maxPointsCount = pointsCount;
+                    var pointsCount = 0;
+                    var (otherX, otherY) = GetCords(points[j]);
+
+                    if (currentX == otherX && currentY == otherY)
+                    {
+                        continue;
+                    }
+
+                    pointsCount = GetPointsCountOnTheSameLine(currentX, currentY, otherX, otherY, points);
+
+                    if (pointsCount > maxPointsCount)
+                    {
+                        maxPointsCount = pointsCount;
+                    }
                 }
             }
 
             return maxPointsCount;
         }
 
-        static int FindLineForPoint(int[] point, int[][] points, int patternX, int patternY, int maxPlaneLimit,
-            int minPlaneLimit)
+        static int GetPointsCountOnTheSameLine(int x1, int y1, int x2, int y2, int[][] points)
         {
-            var (currentX, currentY) = GetCords(point);
-            var pointsCount = 1;
-
+            var pointsCount = 2;
             for (var i = 0; i < points.Length; i++)
             {
-                var otherPoint = points[i];
-                var (otherX, otherY) = GetCords(otherPoint);
+                var (x, y) = GetCords(points[i]);
 
-                if (currentX == otherX && currentY == otherY)
+                if ((x == x1 && y == y1) || (x == x2 && y == y2))
                 {
                     continue;
                 }
 
-                if (patternX == 0 && patternY == 0)
+                if (OnTheSameLine(x1, y1, x2, y2, x, y))
                 {
-                    patternX = otherX - currentX;
-                    patternY = otherY - currentY;
-
-                    var currentPointsCount =
-                        FindLineForPoint(otherPoint, points, patternX, patternY, maxPlaneLimit, minPlaneLimit);
-
-                    currentPointsCount++;
-                    if (currentPointsCount > pointsCount)
-                    {
-                        pointsCount = currentPointsCount;
-                    }
-
-                    patternX = 0;
-                    patternY = 0;
-                }
-                else
-                {
-                    var tempPatternX = patternX;
-                    var tempPatternY = patternY;
-
-                    var xterm = 0;
-                    var yterm = 0;
-
-                    if (patternX < 0)
-                    {
-                        xterm = -1;
-                    }
-                    else if (patternX > 0)
-                    {
-                        xterm = 1;
-                    }
-
-                    if (patternY < 0)
-                    {
-                        yterm = -1;
-                    }
-                    else if (patternY > 0)
-                    {
-                        yterm = 1;
-                    }
-
-                    while (true)
-                    {
-                        var nextPossibleX = currentX + tempPatternX;
-                        var nextPossibleY = currentY + tempPatternY;
-
-                        if (nextPossibleX > maxPlaneLimit || nextPossibleX < minPlaneLimit)
-                        {
-                            break;
-                        }
-
-                        if (nextPossibleY > maxPlaneLimit || nextPossibleY < minPlaneLimit)
-                        {
-                            break;
-                        }
-
-                        if (nextPossibleX == otherX && nextPossibleY == otherY)
-                        {
-                            pointsCount++;
-                        }
-
-                        tempPatternX += xterm;
-                        tempPatternY += yterm;
-                    }
+                    pointsCount++;
                 }
             }
 
             return pointsCount;
         }
+        
+        static bool OnTheSameLine(decimal x1, decimal y1, decimal x2, decimal y2, decimal x3, decimal y3)
+        {
+            return 1m / 2m * ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) == 0;
+        }
 
         static (int, int) GetCords(int[] point)
         {
-            const int limitForCords = 10 * 10 * 10 * 10;
-
-            ValidateParam(point.Length, 0, 2);
-
             var x = point[0];
             var y = point[1];
 
-            ValidateParam(x, -limitForCords, limitForCords);
-            ValidateParam(y, -limitForCords, limitForCords);
-
             return (x, y);
-        }
-
-        static void ValidateParam(int param, int min, int max)
-        {
-            if (param < min || param > max)
-            {
-                throw new ArgumentOutOfRangeException(
-                    $"Length should be more than or equal {min} or less than or equal {max}");
-            }
-        }
-
-        static (int, int) FindMaxAndMinLimitsOfPlane(int[][] points)
-        {
-            var max = -10 * 10 * 10 * 10;
-            var min = 10 * 10 * 10 * 10;
-
-            for (var i = 0; i < points.Length; i++)
-            {
-                var (x, y) = GetCords(points[i]);
-
-                if (x >= y)
-                {
-                    if (x > max)
-                    {
-                        max = x;
-                    }
-
-                    if (y < min)
-                    {
-                        min = y;
-                    }
-                }
-                else
-                {
-                    if (y > max)
-                    {
-                        max = y;
-                    }
-
-                    if (x < min)
-                    {
-                        min = x;
-                    }
-                }
-            }
-
-            return (max, min);
         }
     }
 }
